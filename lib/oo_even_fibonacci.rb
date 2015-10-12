35a4b8a4929aafcbb92b80bd7b2a744f81e0bda8
rolandobrown <rolando.brown@gmail.com>
2015-10-12 17:20:40 -0400

    return sum of fibonacci

--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,12 @@
-# Implement your object-oriented solution here!
+#Object Oriented
+class EvenFibonacci
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+    array = [0, 1]
+    array << (array[-1] + array[-2]) while (array[-1] + array[-2]) < @limit
+    array.select(&:even?).reduce(:+)
+  end
+end
